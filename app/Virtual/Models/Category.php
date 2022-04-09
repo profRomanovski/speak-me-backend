165c7daa2b04b7acad260ddf62502f86ba99e6a8
Roman Gurniy <roman.gurniy@monogo.pl>
2022-04-09 23:08:50 +0300

    Add categories

---
+++ app/Virtual/Models/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Virtual\Models;
+/**
+ * @OA\Schema(
+ *     title="User",
+ *     description="Project user",
+ *     @OA\Xml(
+ *         name="User"
+ *     )
+ * )
+ */
+class User
+{
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID",
+     *     format="int64",
+     *     example=1
+     * )
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @OA\Property(
+     *      title="Name",
+     *      description="Name of the new user",
+     *      example="Roman Gurniy"
+     * )
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @OA\Property(
+     *      title="Email",
+     *      description="Email of the new user",
+     *      example="roman@gmail.com"
+     * )
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @OA\Property(
+     *      title="Password",
+     *      description="Password of the new user",
+     *      example="secret123A"
+     * )
+     *
+     * @var string
+     */
+    public $password;
+}
